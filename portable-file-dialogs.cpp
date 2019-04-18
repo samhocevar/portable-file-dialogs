7dda7cd31939cc32cfce581bac2958c0919e82e2
CCHyper <12141592+CCHyper@users.noreply.github.com>
2019-04-18 20:19:55 +0100

    Adds dummy .cpp file and simple CMake build script.

---
+++ portable-file-dialogs.cpp
@@ -0,0 +1,14 @@
+//
+//  Portable File Dialogs
+//
+//  Copyright © 2018—2019 Sam Hocevar <sam@hocevar.net>
+//
+//  This library is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
+//
+
+// NOTE: this a dummy file added to allow cmake build this as a seperate project.
+#include "portable-file-dialogs.h"
