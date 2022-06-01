69e1067b64af2198c485f95544dd04f31fb5efe0
Sam Hocevar <sam@hocevar.net>
2022-06-01 11:51:17 +0200

    Adapt OS X save_file() when default_path is a file instead of a directory (Closes #65)

--- portable-file-dialogs.h
+++ portable-file-dialogs.h
@@ -1223,7 +1223,13 @@ inline internal::file_dialog::file_dialog(type in_type,
         }
 
         if (default_path.size())
-            script += " default location " + osascript_quote(default_path);
+        {
+            if (in_type == type::folder || is_directory(default_path))
+                script += " default location " + osascript_quote(default_path);
+            else
+                script += " default name " + osascript_quote(default_path);
+        }
+
         script += " with prompt " + osascript_quote(title);
 
         if (in_type == type::open)
